00549eaa50e93b008bf242f0095024365efe4776
superr4y <superr4y@gmail.com>
2016-07-23 17:40:27 +0200

    rest api get/set position and add TODO.txt

--- rest_server.py
+++ rest_server.py
@@ -1,25 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 api = None
 
-@app.route("/inventory")
+@app.route("/inventory", methods=["GET"])
 def get_inventory():
-    response = api.get_inventory().call()
-    if response and ('GET_INVENTORY' in response['responses']):
-        inventory_items = response['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']
-        return jsonify(inventory_items)
-    return 'Error in GET_INVENTORY reponse'
+    inventory_items = api.get_inventory().call()['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']
+    return jsonify(inventory_items)
 
+@app.route("/position", methods=["GET"])
+def get_position():
+    lat, lng, alt = api.get_position()
+    pos = {"lat": lat, "lng": lng, "alt": alt}
+    return jsonify(pos)
 
-@app.route("/player")
-def get_player():
-    response = api.get_player().call()
-    if response and response['responses']['GET_PLAYER']['success'] == True:
-        player = response['responses']['GET_PLAYER']['player_data']
-        return jsonify(player)
-    return 'Error in GET_PLAYER reponse'
-
-
+@app.route("/position", methods=["POST"])
+def set_position():
+    try:
+        lat = request.form.get('lat', type=float)
+        lng = request.form.get('lng', type=float)
+        alt = request.form.get('alt', type=float)
+        api.set_position(lat, lng, alt)
+        return jsonify({"status": "ok"})
+    except:
+        return jsonify({"status": "error"})
 
 if __name__ == "__main__":
     app.run()
