2810b264a634107d8a7671034b39dd36d92cd045
station <egal@egal.egal>
2016-07-23 16:16:33 +0200

    added get_player route

--- rest_server.py
+++ rest_server.py
@@ -4,8 +4,21 @@ api = None
 
 @app.route("/inventory")
 def get_inventory():
-    inventory_items = api.get_inventory().call()['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']
-    return jsonify(inventory_items)
+    response = api.get_inventory().call()
+    if response and ('GET_INVENTORY' in response['responses']):
+        inventory_items = response['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']
+        return jsonify(inventory_items)
+    return 'Error in GET_INVENTORY reponse'
+
+
+@app.route("/player")
+def get_player():
+    response = api.get_player().call()
+    if response and response['responses']['GET_PLAYER']['success'] == True:
+        player = response['responses']['GET_PLAYER']['player_data']
+        return jsonify(player)
+    return 'Error in GET_PLAYER reponse'
+
 
 
 if __name__ == "__main__":
